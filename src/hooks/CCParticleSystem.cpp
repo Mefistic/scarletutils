ae31cb3080c07b126059bd2e875a7a49c476c3d1
scarlet <mefisticescola@gmail.com>
2026-08-04 13:04:25 -0300

    oops forgot a whole ass file

---
+++ src/hooks/CCParticleSystem.cpp
@@ -0,0 +1,11 @@
+#include "../includes.hpp"
+#include <Geode/modify/CCParticleSystem.hpp>
+
+using namespace geode::prelude;
+
+class $modify(ScarletCCParticleSystem, CCParticleSystem) {
+	void update(float dt) {
+        if (!noEffect)
+        return update(dt);
+    	}
+};
